653a3db0c0e85845db2b612d1d2f00a69658796d
Łukasz Bendykowski <lukasz.bendykowski@gmail.com>
2017-06-10 16:38:47 +0200

    Add 'dotfiles' and 'up' aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,7 +1,23 @@
 #!/usr/bin/env zsh
 
-# find alias
+# Go to main dotfiles directory
+alias dotfiles="cd $DOTFILES"
+
+# Find alias
 falias() {
     params="$@"
     alias | grep "$params"
+}
+
+# Go UP n times: ex. 'up 2' means 'cd ../..'
+up() {
+    if [[ "$#" < 1 ]] ; then
+        cd ..
+    else
+        CDSTR=""
+        for i in {1..$1} ; do
+            CDSTR="../$CDSTR"
+        done
+        cd $CDSTR
+    fi
 }
