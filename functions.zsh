dfc8b77fa069f6497deb23d9f1fd56994af9f948
Łukasz Bendykowski <lukasz.bendykowski@gmail.com>
2017-06-11 22:49:00 +0200

    Add function rename omitted in the recent commit

--- functions.zsh
+++ functions.zsh
@@ -1,6 +1,6 @@
 dotfiles_export_aliases_prettyfied() {
-    if [ -z $DOTFILES_ALIASES_PRETTYFIED ]; then
-        export DOTFILES_ALIASES_PRETTYFIED="$(
+    if [ -z $dotfiles_aliases_prettyfied ]; then
+        export dotfiles_aliases_prettyfied="$(
             echo "$(alias)" | 
             sed "s/'\{0,1\}\([^?']*\)\?\{0,1\}'\{0,1\}='\{0,1\}\(echo ?:\)\{0,1\}\([^']*\)'\{0,1\}/\1 -> \3/g")"
     fi
